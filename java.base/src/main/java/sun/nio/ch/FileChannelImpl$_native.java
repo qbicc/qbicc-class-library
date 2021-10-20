0b7befae8e14fa658581b41f83e16d9078301fd4
David Grove <groved@us.ibm.com>
2021-10-20 11:06:56 -0400

    stub out sun.nio.ch.FileChannelImpl.initIDs

---
+++ java.base/src/main/java/sun/nio/ch/FileChannelImpl$_native.java
@@ -0,0 +1,7 @@
+package sun.nio.ch;
+
+public class FileChannelImpl$_native {
+    public static long initIDs() {
+        return 4096; // TODO: This is supposed to be the value of _SC_PAGESIZE
+    }
+}
