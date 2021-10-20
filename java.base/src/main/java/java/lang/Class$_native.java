7e38b3c351ab06aca7cc50e4fa382eb03245e936
David Grove <groved@us.ibm.com>
2021-10-20 16:49:16 -0400

    stub a few native methods of Class

---
+++ java.base/src/main/java/java/lang/Class$_native.java
@@ -0,0 +1,20 @@
+package java.lang;
+
+
+// TODO: These are all stubs so we don't turn them into
+//       an UnsatisfiedLinkError before the interpreter can
+//       intercept them at build time.
+//       Eventually we need a real runtime implementation too...
+public final class Class$_native<T> {
+    public boolean isAssignableFrom(Class<?> cls) {
+        throw new UnsupportedOperationException();
+    }
+
+    public Class<?> getSuperclass() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getModifiers() {
+        throw new UnsupportedOperationException();
+    }
+}
