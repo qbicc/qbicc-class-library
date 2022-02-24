306359555ea8d17977f8981f8a6ee8c99f544111
David M. Lloyd <david.lloyd@redhat.com>
2022-02-24 16:32:56 -0600

    Fix a bug where overlapping arraycopy can malfunction

--- java.base/src/main/java/java/lang/System$_native.java
+++ java.base/src/main/java/java/lang/System$_native.java
@@ -52,41 +52,108 @@ public final class System$_native {
     // Temporary manual implementation
     @SuppressWarnings("ManualArrayCopy")
     public static void arraycopy(Object src, int srcPos, Object dest, int destPos, int length) {
+        if (src == dest && srcPos == destPos) {
+            // no operation
+            return;
+        }
         if (src instanceof Object[] srcArray && dest instanceof Object[] destArray) {
-            for (int i = 0; i < length; i ++) {
-                destArray[destPos + i] = srcArray[srcPos + i];
+            if (src == dest && destPos > srcPos) {
+                // copy backwards
+                for (int i = length - 1; i >= 0; i --) {
+                    destArray[destPos + i] = srcArray[srcPos + i];
+                }
+            } else {
+                for (int i = 0; i < length; i ++) {
+                    destArray[destPos + i] = srcArray[srcPos + i];
+                }
             }
         } else if (src instanceof byte[] srcArray && dest instanceof byte[] destArray) {
-            for (int i = 0; i < length; i ++) {
-                destArray[destPos + i] = srcArray[srcPos + i];
+            if (src == dest && destPos > srcPos) {
+                // copy backwards
+                for (int i = length - 1; i >= 0; i --) {
+                    destArray[destPos + i] = srcArray[srcPos + i];
+                }
+            } else {
+                for (int i = 0; i < length; i ++) {
+                    destArray[destPos + i] = srcArray[srcPos + i];
+                }
             }
         } else if (src instanceof short[] srcArray && dest instanceof short[] destArray) {
-            for (int i = 0; i < length; i ++) {
-                destArray[destPos + i] = srcArray[srcPos + i];
+            if (src == dest && destPos > srcPos) {
+                // copy backwards
+                for (int i = length - 1; i >= 0; i --) {
+                    destArray[destPos + i] = srcArray[srcPos + i];
+                }
+            } else {
+                for (int i = 0; i < length; i ++) {
+                    destArray[destPos + i] = srcArray[srcPos + i];
+                }
             }
         } else if (src instanceof int[] srcArray && dest instanceof int[] destArray) {
-            for (int i = 0; i < length; i ++) {
-                destArray[destPos + i] = srcArray[srcPos + i];
+            if (src == dest && destPos > srcPos) {
+                // copy backwards
+                for (int i = length - 1; i >= 0; i --) {
+                    destArray[destPos + i] = srcArray[srcPos + i];
+                }
+            } else {
+                for (int i = 0; i < length; i ++) {
+                    destArray[destPos + i] = srcArray[srcPos + i];
+                }
             }
         } else if (src instanceof long[] srcArray && dest instanceof long[] destArray) {
-            for (int i = 0; i < length; i ++) {
-                destArray[destPos + i] = srcArray[srcPos + i];
+            if (src == dest && destPos > srcPos) {
+                // copy backwards
+                for (int i = length - 1; i >= 0; i --) {
+                    destArray[destPos + i] = srcArray[srcPos + i];
+                }
+            } else {
+                for (int i = 0; i < length; i ++) {
+                    destArray[destPos + i] = srcArray[srcPos + i];
+                }
             }
         } else if (src instanceof char[] srcArray && dest instanceof char[] destArray) {
-            for (int i = 0; i < length; i ++) {
-                destArray[destPos + i] = srcArray[srcPos + i];
+            if (src == dest && destPos > srcPos) {
+                // copy backwards
+                for (int i = length - 1; i >= 0; i --) {
+                    destArray[destPos + i] = srcArray[srcPos + i];
+                }
+            } else {
+                for (int i = 0; i < length; i ++) {
+                    destArray[destPos + i] = srcArray[srcPos + i];
+                }
             }
         } else if (src instanceof float[] srcArray && dest instanceof float[] destArray) {
-            for (int i = 0; i < length; i ++) {
-                destArray[destPos + i] = srcArray[srcPos + i];
+            if (src == dest && destPos > srcPos) {
+                // copy backwards
+                for (int i = length - 1; i >= 0; i --) {
+                    destArray[destPos + i] = srcArray[srcPos + i];
+                }
+            } else {
+                for (int i = 0; i < length; i ++) {
+                    destArray[destPos + i] = srcArray[srcPos + i];
+                }
             }
         } else if (src instanceof double[] srcArray && dest instanceof double[] destArray) {
-            for (int i = 0; i < length; i ++) {
-                destArray[destPos + i] = srcArray[srcPos + i];
+            if (src == dest && destPos > srcPos) {
+                // copy backwards
+                for (int i = length - 1; i >= 0; i --) {
+                    destArray[destPos + i] = srcArray[srcPos + i];
+                }
+            } else {
+                for (int i = 0; i < length; i ++) {
+                    destArray[destPos + i] = srcArray[srcPos + i];
+                }
             }
         } else if (src instanceof boolean[] srcArray && dest instanceof boolean[] destArray) {
-            for (int i = 0; i < length; i ++) {
-                destArray[destPos + i] = srcArray[srcPos + i];
+            if (src == dest && destPos > srcPos) {
+                // copy backwards
+                for (int i = length - 1; i >= 0; i --) {
+                    destArray[destPos + i] = srcArray[srcPos + i];
+                }
+            } else {
+                for (int i = 0; i < length; i ++) {
+                    destArray[destPos + i] = srcArray[srcPos + i];
+                }
             }
         } else {
             throw new ClassCastException("Invalid array types for copy");
