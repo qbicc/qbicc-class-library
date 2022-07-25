507eda8472dbf738dad6266b105af6f55f550532
Edoardo Vacchi <evacchi@users.noreply.github.com>
2022-07-25 17:20:59 +0200

    Use IsPThreads

--- java.base/src/main/java/java/lang/Thread$_patch.java
+++ java.base/src/main/java/java/lang/Thread$_patch.java
@@ -84,13 +84,13 @@ public class Thread$_patch {
     native void setPriority(final int priority);
     static native long nextThreadID();
 
-    @Add(unless = { Build.Target.IsWasm.class })
+    @Add(when = Build.Target.IsPThreads.class)
     pthread_t thread;
 
     // used only by non-Linux
-    @Add(unless = { Build.Target.IsLinux.class, Build.Target.IsWasm.class })
+    @Add(when = Build.Target.IsPThreads.class, unless = Build.Target.IsLinux.class)
     pthread_mutex_t mutex;
-    @Add(unless = { Build.Target.IsLinux.class, Build.Target.IsWasm.class })
+    @Add(when = Build.Target.IsPThreads.class, unless = Build.Target.IsWasm.class)
     pthread_cond_t cond;
 
     @Replace
@@ -127,7 +127,7 @@ public class Thread$_patch {
     public void initializeNativeFields() {
         // initialize native fields
         threadStatus = 0;
-        if (Build.isTarget() && ! Build.Target.isLinux() && !Build.Target.isWasm()) {
+        if (Build.isTarget() && Build.Target.isPThreads() && ! Build.Target.isLinux()) {
             // mutex type does not matter
             c_int res = pthread_mutex_init(addr_of(refToPtr(this).sel().mutex), zero());
             if (res.isNonNull()) {
@@ -234,7 +234,7 @@ public class Thread$_patch {
     @Hidden
     @NoReflect
     private void start0() {
-        if (Build.Target.isWasm()) {
+        if (!Build.Target.isPThreads()) {
             return;
         }
         // initialize mutex & condition if there is one
@@ -326,7 +326,7 @@ public class Thread$_patch {
     @SuppressWarnings("ConstantConditions")
     @Add
     static void park(boolean isAbsolute, long time) {
-        if (Build.Target.isWasm()) {
+        if (!Build.Target.isPThreads()) {
             return;
         }
         Thread thread = Thread.currentThread();
@@ -422,7 +422,7 @@ public class Thread$_patch {
 
     @Add
     void unpark() {
-        if (Build.Target.isWasm()) {
+        if (!Build.Target.isPThreads()) {
             return;
         }
         int32_t_ptr ptr = addr_of(refToPtr(this).sel().threadStatus).cast();
