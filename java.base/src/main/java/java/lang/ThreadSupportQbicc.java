f65bdcac9fe28710e61da290c70478e042fdf2e4
David Grove <groved@us.ibm.com>
2022-09-02 12:04:04 -0400

    rename exported symobols to avoid clash during migration

--- java.base/src/main/java/java/lang/ThreadSupportQbicc.java
+++ java.base/src/main/java/java/lang/ThreadSupportQbicc.java
@@ -8,12 +8,8 @@ import org.qbicc.runtime.ThreadScoped;
  * has some gaps that make delcaring them in a separate class expedient.
  *   1. @ThreadScoped fields can't be declated on Thrad (circular dependency)
  *   2. The @Add annotation doesn't preserve the other annotations (like @export).
- *
- *   TODO: These declarations are temporarily public to enable the multiple steps
- *          of migrating the code from org.qbicc.runtime.main.VM to here.
- *          We can make them package-scoped when the migration has completed.
  */
-public class ThreadSupportQbicc {
+class ThreadSupportQbicc {
 
     /**
      * Internal holder for the pointer to the current thread.
@@ -24,19 +20,19 @@ public class ThreadSupportQbicc {
     @ThreadScoped
     @export
     @SuppressWarnings("unused")
-    public static Thread _qbicc_bound_thread;
+    static Thread _qbicc_bound_java_thread;
 
     /**
      * A pointer to this function is passed to pthread_create by start0.
      * The role of this wrapper is to transition a newly started Thread from C to Java calling
-     * conventions and then invoke the Thread's run method.
+     * conventions and then invoke the Thread's run0 method.
      * @param threadParam - java.lang.Thread object for the newly started thread (cast to a void pointer to be compatible with pthread_create)
      * @return null - this return value will not be used
      */
     @export
-    public static void_ptr pthreadCreateWrapper(void_ptr threadParam) {
+    static void_ptr qbiccThreadRunWrapper(void_ptr threadParam) {
         Object thrObj = ptrToRef(threadParam);
-        _qbicc_bound_thread = (Thread)thrObj;
+        _qbicc_bound_java_thread = (Thread)thrObj;
         ((Thread$_patch)thrObj).run0();
         return word(0).cast();
     }
