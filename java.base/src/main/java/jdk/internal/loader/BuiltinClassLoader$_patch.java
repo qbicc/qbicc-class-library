4569d58d8fb9dec8b6c242a05abd7203d3149256
David Grove <groved@us.ibm.com>
2022-10-21 10:42:03 -0400

    support runtime resource loading via nativeimage URL

---
+++ java.base/src/main/java/jdk/internal/loader/BuiltinClassLoader$_patch.java
@@ -0,0 +1,64 @@
+/*
+ * This code is based on OpenJDK source file(s) which contain the following copyright notice:
+ *
+ * ------
+ * Copyright (c) 2001, 2021, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ * ------
+ *
+ * This file may contain additional modifications which are Copyright (c) Red Hat and other
+ * contributors.
+ */
+
+package jdk.internal.loader;
+
+import java.net.URL;
+import java.util.Collections;
+import java.util.Enumeration;
+
+import org.qbicc.rt.annotation.Tracking;
+import org.qbicc.runtime.Build;
+import org.qbicc.runtime.patcher.PatchClass;
+import org.qbicc.runtime.patcher.Replace;
+
+@PatchClass(BuiltinClassLoader.class)
+@Tracking("src/java.base/share/classes/jdk/internal/loader/BuiltinClassLoader.java")
+public class BuiltinClassLoader$_patch {
+    @Replace
+    private URL findResourceOnClassPath(String name) {
+        if (Build.isHost()) {
+            // This method is hooked in the interpreter.  We should not be here!
+            throw new IllegalStateException("findResourceOnClassPath: interpreting runtime version");
+        }
+        return NativeImageResources.findResource((ClassLoader)(Object)this, name);
+    }
+
+    @Replace
+    private Enumeration<URL> findResourcesOnClassPath(String name) {
+        if (Build.isHost()) {
+            // This method is hooked in the interpreter.  We should not be here!
+            throw new IllegalStateException("findResourceOnClassPath: interpreting runtime version");
+        }
+        return NativeImageResources.findResources((ClassLoader)(Object)this, name);
+    }
+}
