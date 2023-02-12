3afd4cf1a581f0730fa708f21b43454880ba5e3c
David Grove <groved@us.ibm.com>
2023-02-12 16:10:27 -0500

    implement sun.nio.FileChannelImpl.transferTo0

--- java.base/src/main/java/sun/nio/ch/FileChannelImpl$_native.java
+++ java.base/src/main/java/sun/nio/ch/FileChannelImpl$_native.java
@@ -32,6 +32,16 @@
 
 package sun.nio.ch;
 
+import static org.qbicc.runtime.CNative.*;
+import static org.qbicc.runtime.bsd.SysSocket.*;
+import static org.qbicc.runtime.linux.SysSendfile.*;
+import static org.qbicc.runtime.posix.Errno.*;
+import static org.qbicc.runtime.posix.SysTypes.*;
+import static org.qbicc.runtime.posix.Unistd.*;
+import static org.qbicc.runtime.stdc.Errno.*;
+
+import java.io.FileDescriptor;
+import java.io.IOException;
 import org.qbicc.rt.annotation.Tracking;
 import org.qbicc.runtime.Build;
 
@@ -49,6 +59,51 @@ public class FileChannelImpl$_native {
             return Integer.MAX_VALUE-1;
         } else {
             return Integer.MAX_VALUE;
+        }
+    }
+
+    private long transferTo0(FileDescriptor src, long position, long count, FileDescriptor dst) throws IOException {
+        c_int srcFD = word(((FileDescriptor$_aliases)(Object)src).fd);
+        c_int dstFD = word(((FileDescriptor$_aliases)(Object)dst).fd);
+
+        if (Build.Target.isLinux()) {
+            off_t offset = auto(word(position));
+            ssize_t n = sendfile(dstFD, srcFD, addr_of(offset), word(count));
+            if (n.longValue() < 0) {
+                if (errno == EAGAIN.intValue()) {
+                    return IOStatus.UNAVAILABLE;
+                } else if (errno == EINVAL.intValue() && count >= 0) {
+                    return IOStatus.UNSUPPORTED_CASE;
+                } else if (errno == EINTR.intValue()) {
+                    return IOStatus.INTERRUPTED;
+                } else {
+                    throw new IOException("Transfer failed");
+                }
+            }
+            return n.longValue();
+        } else if (Build.Target.isMacOs()) {
+            c_int result;
+            off_t numBytes = auto(word(count));
+            result = sendfile(srcFD, dstFD, word(position), addr_of(numBytes), word(0), word(0));
+            if (numBytes.longValue() > 0) {
+                return numBytes.longValue();
+            }
+            if (result.intValue() == -1) {
+                if (errno == EAGAIN.intValue()) {
+                    return IOStatus.UNAVAILABLE;
+                } else if (errno == EOPNOTSUPP.intValue() || errno == ENOTSOCK.intValue() || errno == ENOTCONN.intValue()) {
+                    return IOStatus.UNSUPPORTED_CASE;
+                } else if (errno == EINVAL.intValue() && count >= 0) {
+                    return IOStatus.UNSUPPORTED_CASE;
+                } else if (errno == EINTR.intValue()) {
+                    return IOStatus.INTERRUPTED;
+                } else {
+                    throw new IOException("Transfer failed");
+                }
+            }
+            return result.longValue();
+        } else {
+            return IOStatus.UNSUPPORTED_CASE;
         }
     }
 }
